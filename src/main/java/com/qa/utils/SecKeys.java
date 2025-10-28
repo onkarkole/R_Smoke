783685c5618da730b3ece65e80cdfd4ddb45f7d9
Onkar Kole <41734467+onkarkole@users.noreply.github.com>
2025-10-28 12:17:42 +0530

    config updated

--- src/main/java/com/qa/utils/SecKeys.java
+++ src/main/java/com/qa/utils/SecKeys.java
@@ -4,20 +4,20 @@ public final class SecKeys {
     private SecKeys() {}
 
     // Encrypted key tokens (no real names visible)
-    public static final String ADMIN_USERNAME = "K|95a8f1d2a13b056aa5f3c310236de390c42d74579f64134e6198aaae4d50e9b0";
-    public static final String TRAINER_USERNAME = "K|eecf14a32ae9ef29873df2c5701788e8005af8b86608e8ff2a2259795deb7fa2";
-    public static final String EMAIL_CC = "K|e1af9e31d1c0ae5fbe430e7ec702f08dbe60b2f42a6f82e5b3f590fa006352ee";
-    public static final String MAIL_PASSWORD = "K|8442328146e02dc22dfe10a77b5b71b0fcc37e7ca6481e184071ae9c258257c3";
-    public static final String MAIL_PROVIDER = "K|5ac9499da08dfd10d72b0d4b9da7d2c732f8dbca34fb68d4498b7e44267d766f";
-    public static final String MAIL_TO = "K|8ebbde424943ca7b70621d36d37b1c061e3bec3c1fa55874091627d8740b5458";
-    public static final String MAIL_USERNAME = "K|8a6ac7cc34eac3da1697a4740d972b8b773de9c4a516b24cf8556f0401aac30f";
-    public static final String OTP_SENDEREMAIL = "K|d76f14ade89b8711561f416eaaf07914a4b93a7ccf3b27e0764a31cd18d865a0";
-    public static final String OTP_SUBJECT = "K|1b4dd574e74ecfae85deaf32d308111c1a2fd7ee71f94e5bf23bc6d4e66cd145";
-    public static final String OTP_LENGTH = "K|74d94fd644b854e3ce9463fccb23f3c468002a7061d880703541018eaa2c29a3";
-    public static final String OTP_PREFERLAST = "K|bc821842451ff8c811aeb1a1ae62311b6e43863eb781a6d0c685078e70bf07ba";
-    public static final String APPLICATIONBASEURL = "K|ea1c4e697aadfc2207779f6385150494542a146356df0c4adad5bd24ebb38f54";
-    public static final String ADMINPORTAL = "K|cba05621b16a1e6ebc1545f2e10f1383dc955096cb58a8b96698c1012dfbc01f";
-    public static final String TRAINERPORTAL = "K|d52cc1ae72396479cf3be3e6b4cd29efcf3b90075fbf77a8ec5b762b637a2f32";
-    public static final String BENEFICIARYPORTAL = "K|da30a3f95c242ea62884d61fb1a3e6233d0d08f57ef9e5c92d3a30b667b79e52";
-    public static final String OTP_TAIL_WINDOW = "K|3858c2235b9247200050064f78504fa9b38fdf7d1e528da27c48056c61a9cd1a";
+    public static final String ADMIN_USERNAME = "K|f4a869b96e4a7bfa635501b5073ab86c5965a54cec9412517e7344c33acc9f6a";
+    public static final String TRAINER_USERNAME = "K|39eb1cd27aee83fb4a06e557d66cdb318d1372f3fe28c3bc5f4213d5942465e0";
+    public static final String EMAIL_CC = "K|b5ed464cb2e83b1feaf40d42602df727f97077092f1bab8c26afa04604ab7cf0";
+    public static final String MAIL_PASSWORD = "K|41c738efbfbe1d06fb6be217a33dc83ec8fd8eed9e82c8053154137948cec7ed";
+    public static final String MAIL_PROVIDER = "K|a1245feb3cd73276baae90c7b76a1587c56ccf47738436a41cb67683d94c87f5";
+    public static final String MAIL_TO = "K|98279633861d3c75db1a3385fd170c4d10c35b52ddd446285c66f54db30384ab";
+    public static final String MAIL_USERNAME = "K|b702f6e7ff040d9d2e9267d6abc3a75ee3ecdf3201d5839e65dd8e59d2861d99";
+    public static final String OTP_SENDEREMAIL = "K|b09d5d268d46c72e144aa744a3c0537d0bc2404ea3bedc8fb5b5bfc58f85eb53";
+    public static final String OTP_SUBJECT = "K|8f5436437a825f2e66574d9037c78fbfd971854973a2543bd2eeae98255631b7";
+    public static final String OTP_LENGTH = "K|4768e97a906605a901f3eba60886b1474319b51c30555d236a4dfe2beb6164e9";
+    public static final String OTP_PREFERLAST = "K|4f64139c18960c88b1bd2cd260bf86680133ee19d1b315c27598907970e5f539";
+    public static final String APPLICATIONBASEURL = "K|863bdcb1b02084c35eb88e12f53ea1c481d6e9fb62627938208a5319645f48ba";
+    public static final String ADMINPORTAL = "K|fd4f66e2673c2a6c57ceaf35b2e7c92071882e1cd0bc8b7891ffd6fca268c089";
+    public static final String TRAINERPORTAL = "K|21a2c31ffb5e042518c84619feaecf695a8ed7a85dbb629fc695f9f9299f6f7f";
+    public static final String BENEFICIARYPORTAL = "K|1da39942669b5528d5cac46f850ee27eb29b740c49996db18a19b590f2a48025";
+    public static final String OTP_TAIL_WINDOW = "K|2924b666865e26cbf56b28a4fad9c92bfbf1d6f43c3f1a8f95df99f0dbad32f2";
 }
